a34d1cdcf483043aa521d0ad6478755e3f9e546b
Teagan glenn <teagan.m.glenn@gmail.com>
2020-06-23 22:26:57 -0600

    Initial commit

---
+++ src/Photon-MQTT-Garage-Door-Controller.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/tglenn/src/embedded/Photon-MQTT-Garage-Door-Controller/src/Photon-MQTT-Garage-Door-Controller.ino"
+/*
+ * Project Photon-MQTT-Garage-Door-Controller
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/tglenn/src/embedded/Photon-MQTT-Garage-Door-Controller/src/Photon-MQTT-Garage-Door-Controller.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
